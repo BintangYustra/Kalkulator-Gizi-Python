3d6b13cc62f48156440bc0d43e55f371846cdf77
BintangYustra <bintangyustrawartana@gmail.com>
2026-07-16 11:51:29 +0800

    menyederhanakan sistem

--- index.py
+++ index.py
@@ -5,66 +5,59 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics.pairwise import cosine_similarity
 
-st.set_page_config(page_title="Kalkulator Gizi", page_icon="🥗", layout="wide")
-# rubah icon pake custom made nanti
+st.set_page_config(page_title="Kalkulator Gizi", page_icon="LogoKalkulator.png", layout="wide")
 st.image("BannerSmall2.png") 
-
 st.title("Kalkulator Gizi Makanan")
 
-# Deklarasi
+
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
-tab1, tab2 = st.tabs(["Profil", "Pilih Makanan"])
 
-with tab1:
-    jenis_kelamin = st.selectbox("Pilih Jenis Kelamin", ["---", "Pria", "Wanita"])
+jenis_kelamin = st.selectbox("Pilih Jenis Kelamin", ["---", "Pria", "Wanita"])
 
-    if jenis_kelamin == "---":
-        st.write("Pilih Jenis Kelamin")
-    else:
-        st.subheader("Masukkan Informasi Singkat Mengenai Anda")
-        umur = st.number_input("Umur = ", 0)
-        tinggi_badan = st.number_input("Tinggi Badan(CM) = ", 0)
-        berat_badan = st.number_input("Berat Badan(KG) = ", 0)
+if jenis_kelamin == "---":
+    st.write("Pilih Jenis Kelamin")
+else:
+    st.subheader("Masukkan Informasi Singkat Mengenai Anda")
+    umur = st.number_input("Umur = ", 0)
+    tinggi_badan = st.number_input("Tinggi Badan(CM) = ", 0)
+    berat_badan = st.number_input("Berat Badan(KG) = ", 0)
 
-        if jenis_kelamin == "Pria":
-            bmr = (10 * berat_badan) + (6.25 * tinggi_badan) - (5 * umur) + 5
+    if jenis_kelamin == "Pria":
+        bmr = (10 * berat_badan) + (6.25 * tinggi_badan) - (5 * umur) + 5
 
-        else: # buat wanita
-            bmr = (10 * berat_badan) + (6.25 * tinggi_badan) - (5 * umur) - 161
+    else: # buat wanita
+        bmr = (10 * berat_badan) + (6.25 * tinggi_badan) - (5 * umur) - 161
 
-        st.metric(label="BMR Anda", value=f"{bmr} kkal/hari")
+    st.metric(label="BMR Anda", value=f"{bmr} kkal/hari")
 
-        tingkat_aktivitas = st.selectbox("Tipe Aktivitas = ", 
-                                        ["---",
-                                        "Jarang gerak/duduk terus",
-                                        "Aktivitas ringan (olahraga 1-3 kali/minggu)",
-                                        "Aktivitas sedang (olahraga 3-5 kali/minggu)",
-                                        "Aktivitas berat (olahraga 6-7 kali/minggu)",
-                                        "Aktivitas sangat berat (olahraga intens 2x sehari)"
-                                        ])
-        if tingkat_aktivitas == "---":
-            st.write("---")
+    tingkat_aktivitas = st.selectbox("Tipe Aktivitas = ", 
+                                    ["---",
+                                    "Jarang gerak/duduk terus",
+                                    "Aktivitas ringan (olahraga 1-3 kali/minggu)",
+                                    "Aktivitas sedang (olahraga 3-5 kali/minggu)",
+                                    "Aktivitas berat (olahraga 6-7 kali/minggu)",
+                                    "Aktivitas sangat berat (olahraga intens 2x sehari)"
+                                    ])
+    if tingkat_aktivitas == "---":
+        st.write("---")
 
-        elif tingkat_aktivitas == "Jarang gerak/duduk terus":
-            tdee = bmr * 1.2
-            st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
+    elif tingkat_aktivitas == "Jarang gerak/duduk terus":
+        tdee = bmr * 1.2
+        st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
 
-        elif tingkat_aktivitas == "Aktivitas ringan (olahraga 1-3 kali/minggu)":
-            tdee = bmr * 1.375
-            st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
+    elif tingkat_aktivitas == "Aktivitas ringan (olahraga 1-3 kali/minggu)":
+        tdee = bmr * 1.375
+        st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
 
-        elif tingkat_aktivitas == "Aktivitas sedang (olahraga 3-5 kali/minggu)":
-            tdee = bmr * 1.55
-            st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
+    elif tingkat_aktivitas == "Aktivitas sedang (olahraga 3-5 kali/minggu)":
+        tdee = bmr * 1.55
+        st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
 
-        elif tingkat_aktivitas == "Aktivitas berat (olahraga 6-7 kali/minggu)":
-            tdee = bmr * 1.725
-            st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
+    elif tingkat_aktivitas == "Aktivitas berat (olahraga 6-7 kali/minggu)":
+        tdee = bmr * 1.725
+        st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
 
-        elif tingkat_aktivitas == "Aktivitas sangat berat (olahraga intens 2x sehari)":
-            tdee = bmr * 1.9
-            st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
-
-with tab2:
-    st.write("Masih Dibuat")
+    elif tingkat_aktivitas == "Aktivitas sangat berat (olahraga intens 2x sehari)":
+        tdee = bmr * 1.9
+        st.metric(label="TDEE Anda", value=f"{tdee} Kalori per Hari")
